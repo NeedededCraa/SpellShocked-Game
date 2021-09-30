1e77afac26bb8a08f1008fda2b9668294e16cd65
s-stewartlu <stewluj@outlook.com>
2021-09-30 13:06:34 -0700

    override if you wish

--- desktop/src/com/spellshocked/game/desktop/DesktopLauncher.java
+++ desktop/src/com/spellshocked/game/desktop/DesktopLauncher.java
@@ -2,11 +2,14 @@ package com.spellshocked.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.spellshocked.game.Spellshocked;
+import com.spellshocked.game.Animator;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new Spellshocked(), config);
+		config.title = "Drop";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new Animator(), config);
 	}
 }
