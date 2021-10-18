a507bbae05ad84402f068c532e36f7c3f0ab72b6
NeedededCraa <davidanicolle@hotmail.com>
2021-10-18 12:37:39 -0700

    new textures

--- core/src/com/spellshocked/game/entity/PlayerEntity.java
+++ core/src/com/spellshocked/game/entity/PlayerEntity.java
@@ -4,8 +4,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class PlayerEntity extends Entity {
-    public static final TextureRegion[][] TEXTURES = TextureRegion.split(new Texture("./images/entities/player.png"), 16, 32);
+    public static final TextureRegion[][] TEXTURES = TextureRegion.split(new Texture("./images/entities/player.png"), 16, 24);
     public static final float WALKSPEED = 1;
+
     public PlayerEntity() {
         super(TEXTURES, WALKSPEED);
     }
