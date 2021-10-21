c57c52efbc6c21c6f81a7e54a11204677eb35a41
s-stewartlu <stewluj@outlook.com>
2021-10-21 12:58:07 -0700

    Fixed float issue with obstacle radius

--- core/src/com/spellshocked/game/gui/PauseGUI.java
+++ core/src/com/spellshocked/game/gui/PauseGUI.java
@@ -1,4 +1,19 @@
 package com.spellshocked.game.gui;
 
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.spellshocked.game.Spellshocked;
+
 public class PauseGUI extends GUI {
+    private Viewport viewport;
+    private Stage stage;
+
+    private Game game;
+
+    public PauseGUI(Game game){
+        this.game = game;
+    }
+
 }
