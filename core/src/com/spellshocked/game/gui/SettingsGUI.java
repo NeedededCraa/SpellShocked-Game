84d78084d4d8398ecdaebb49d6c12aa36a39c135
s-stewartlu <stewluj@outlook.com>
2021-10-29 12:40:42 -0700

    added settings

---
+++ core/src/com/spellshocked/game/gui/SettingsGUI.java
@@ -0,0 +1,10 @@
+package com.spellshocked.game.gui;
+
+import com.spellshocked.game.Spellshocked;
+
+public class SettingsGUI extends GUI{
+    public static final String SKIN = "./pixthulhu/skin/pixthulhu-ui.json";
+    public SettingsGUI(Spellshocked g){
+        super(SKIN);
+    }
+}
