70771e5889c3bb2cc60db52e19288a322ff0ca90
Lucy <65467897+stewluj@users.noreply.github.com>
2022-01-13 13:10:49 -0800

    Skeleton range is wokring and assigns to a new pumpkin

--- core/src/com/spellshocked/game/world/obstacle/Pumpkin.java
+++ core/src/com/spellshocked/game/world/obstacle/Pumpkin.java
@@ -6,20 +6,18 @@ import com.spellshocked.game.gui.ClickGUI;
 import com.spellshocked.game.gui.PickupGUI;
 import com.spellshocked.game.item.Item;
 import com.spellshocked.game.item.PumpkinItem;
+import com.spellshocked.game.world.Tile;
 
 public class Pumpkin extends ObstacleEntity<PickupGUI> {
-    int pumpkinX;
-    int pumpkinY;
+    public Tile tile;
 
     public Pumpkin(PlayerEntity p) {
         super("./json/Object/pumpkin.json", new PickupGUI(p, new PumpkinItem()));
     }
-    public Pumpkin(PlayerEntity p, int x, int y) {
+    public Pumpkin(PlayerEntity p, Tile t) {
         super("./json/Object/pumpkin.json", new PickupGUI(p, new PumpkinItem()));
-        pumpkinX = x;
-        pumpkinY = y;
-
+        tile = t;
     }
-    public int getPumpkinX(){ return pumpkinX;}
-    public int getPumpkinY(){return pumpkinY;}
+    public int getPumpkinX(){ return tile.xValue;}
+    public int getPumpkinY(){return tile.yValue;}
 }
